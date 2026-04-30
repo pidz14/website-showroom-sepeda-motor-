f6df8684738662899e9906a00cb94608076bc004
Jonatan Arya Santosa <2313020222@mhs.univ.ac.id>
2026-04-30 22:37:50 +0700

    fix(frontend): add initial content to views and assets

---
+++ app/Views/layouts/main.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bagong Jaya Motor</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+    <?= $this->renderSection('content') ?>
+    <script src="/assets/js/app.js"></script>
+</body>
+</html>
